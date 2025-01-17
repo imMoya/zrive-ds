a5a1f042ef81dcf75815118f319c5b34c62f8279
Ignacio Moya <imoyared@gmail.com>
2025-01-17 12:04:06 +0100

    style: ruff pass

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -1,145 +1,159 @@
-""" This is a dummy example """
+"""This is a dummy example"""
+
 import os
-import openmeteo_requests
-from openmeteo_sdk.WeatherApiResponse import WeatherApiResponse
-from pathlib import Path
-import matplotlib.pyplot as plt
-import requests_cache
-import pandas as pd
-from retry_requests import retry
-from typing import Dict, List, Optional, Union
-from references import API_URL, COORDINATES, VARIABLES, INI_DATE, END_DATE
 from dataclasses import dataclass
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import openmeteo_requests
+import pandas as pd
+import requests_cache
+from openmeteo_sdk.WeatherApiResponse import WeatherApiResponse
+from references import API_URL, COORDINATES, END_DATE, INI_DATE, VARIABLES
+from retry_requests import retry
+
 
 @dataclass
 class City:
-    name: str
-    latitude: float
-    longitude: float
+	name: str
+	latitude: float
+	longitude: float
+
 
 @dataclass
 class Date:
-    start_date: str
-    end_date: str
+	start_date: str
+	end_date: str
+
 
 class Meteo:
-    def __init__(self, cities: Optional[List[str]]):
-        self.setup()
-        self.cities = cities
+	def __init__(self, cities: list[str] | None):
+		self.setup()
+		self.cities = cities
 
-    @property
-    def date(self):
-        return Date(INI_DATE, END_DATE) 
-    
-    def setup(self) -> None:
-        cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
-        retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
-        self.openmeteo = openmeteo_requests.Client(session = retry_session)
+	@property
+	def date(self):
+		return Date(INI_DATE, END_DATE)
 
-    def get_data_meteo_api(self, city_name: str):
-        city = City(name=city_name, latitude=COORDINATES[city_name]['latitude'], longitude=COORDINATES[city_name]['longitude'])
-        date = self.date
-        params = self.define_params(city, date)
-        self.responses = self.call_api(API_URL, params)
-        return self.responses
-    
-    def call_api(self, api_url, params):
-        # TODO: validate response and handle possible errors
-        return self.openmeteo.weather_api(api_url, params=params)
-    
-    @staticmethod
-    def define_params(
-        city: City,
-        date: Date
-    ) -> Dict: 
-        return {
-            "latitude": city.latitude,
-            "longitude": city.longitude,
-            "start_date": date.start_date,
-            "end_date": date.end_date,
-            "hourly": VARIABLES
-        }
-    
-    @property
-    def data(self):
-        df = pd.DataFrame({})
-        for city in self.cities:
-            df_ = self.get_city_data(city)
-            df = pd.concat([df, df_])
-        return df
-    
-    def get_city_data(self, city: str) -> pd.DataFrame:
-        response = self.get_data_meteo_api(city)[0]
-        df = self.get_hourly_response(response)
-        df = self.compute_additional_params(df)
-        df["city"] = city
-        return df
+	def setup(self) -> None:
+		cache_session = requests_cache.CachedSession('.cache', expire_after=-1)
+		retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+		self.openmeteo = openmeteo_requests.Client(session=retry_session)
 
-    @staticmethod
-    def get_hourly_response(response: WeatherApiResponse) -> pd.DataFrame:
-        hourly = response.Hourly()
-        print(hourly.Interval())
-        hourly_data = {"date": pd.date_range(
-            start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
-            end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
-            freq = pd.Timedelta(seconds = hourly.Interval()),
-            inclusive = "left"
-        )}
-        for i, val in enumerate(VARIABLES):
-            hourly_data[val] = hourly.Variables(i).ValuesAsNumpy()
-        return pd.DataFrame(hourly_data)
-    
-    @staticmethod
-    def compute_additional_params(df: pd.DataFrame) -> pd.DataFrame:
-        df['date'] = pd.to_datetime(df['date'])
-        df['month'] = df['date'].dt.to_period('M')
-        df_monthly = df.groupby('month').agg(
-            temperature_2m_mean=('temperature_2m', 'mean'),
-            precipitation_sum=('precipitation', 'sum'),
-            wind_speed_10m_max=('wind_speed_10m', 'max')
-        ).reset_index()
-        df_monthly['month'] = df_monthly['month'].dt.to_timestamp()
-        return df_monthly
-    
-    @staticmethod
-    def plot_weather_parameter(
-        df: pd.DataFrame, 
-        parameter: str = "temperature_2m_mean", 
-        title: Optional[str] = "",
-        output_filename: Optional[Union[Path, str]] = None
-    ):
-        if parameter not in df.columns:
-            raise ValueError(f"Parameter '{parameter}' not found in DataFrame.")
-        fig = plt.figure(figsize=(16, 8))
-        for city, city_data in df.groupby("city"):
-            plt.plot(
-                city_data["month"], 
-                city_data[parameter], 
-                label=city, 
-                marker='o', 
-                linestyle='-'
-            )
-        plt.title(title if title else f"{parameter.capitalize()} Across Cities", fontsize=16)
-        plt.xlabel("Month", fontsize=14)
-        plt.ylabel(parameter.replace('_', ' ').capitalize(), fontsize=14)
-        plt.legend(title="Cities", fontsize=12)
-        plt.grid()
-        plt.xticks(rotation=45)
-        plt.tight_layout()
-        if output_filename:
-            fig.savefig(output_filename)
+	def get_data_meteo_api(self, city_name: str):
+		city = City(
+			name=city_name,
+			latitude=COORDINATES[city_name]['latitude'],
+			longitude=COORDINATES[city_name]['longitude'],
+		)
+		date = self.date
+		params = self.define_params(city, date)
+		self.responses = self.call_api(API_URL, params)
+		return self.responses
 
-    # TODO: reformat code
+	def call_api(self, api_url, params):
+		# TODO: validate response and handle possible errors
+		return self.openmeteo.weather_api(api_url, params=params)
+
+	@staticmethod
+	def define_params(city: City, date: Date) -> dict:
+		return {
+			'latitude': city.latitude,
+			'longitude': city.longitude,
+			'start_date': date.start_date,
+			'end_date': date.end_date,
+			'hourly': VARIABLES,
+		}
+
+	@property
+	def data(self):
+		df = pd.DataFrame({})
+		for city in self.cities:
+			df_ = self.get_city_data(city)
+			df = pd.concat([df, df_])
+		return df
+
+	def get_city_data(self, city: str) -> pd.DataFrame:
+		response = self.get_data_meteo_api(city)[0]
+		df = self.get_hourly_response(response)
+		df = self.compute_additional_params(df)
+		df['city'] = city
+		return df
+
+	@staticmethod
+	def get_hourly_response(response: WeatherApiResponse) -> pd.DataFrame:
+		hourly = response.Hourly()
+		print(hourly.Interval())
+		hourly_data = {
+			'date': pd.date_range(
+				start=pd.to_datetime(hourly.Time(), unit='s', utc=True),
+				end=pd.to_datetime(hourly.TimeEnd(), unit='s', utc=True),
+				freq=pd.Timedelta(seconds=hourly.Interval()),
+				inclusive='left',
+			)
+		}
+		for i, val in enumerate(VARIABLES):
+			hourly_data[val] = hourly.Variables(i).ValuesAsNumpy()
+		return pd.DataFrame(hourly_data)
+
+	@staticmethod
+	def compute_additional_params(df: pd.DataFrame) -> pd.DataFrame:
+		df['date'] = pd.to_datetime(df['date'])
+		df['month'] = df['date'].dt.to_period('M')
+		df_monthly = (
+			df.groupby('month')
+			.agg(
+				temperature_2m_mean=('temperature_2m', 'mean'),
+				precipitation_sum=('precipitation', 'sum'),
+				wind_speed_10m_max=('wind_speed_10m', 'max'),
+			)
+			.reset_index()
+		)
+		df_monthly['month'] = df_monthly['month'].dt.to_timestamp()
+		return df_monthly
+
+	@staticmethod
+	def plot_weather_parameter(
+		df: pd.DataFrame,
+		parameter: str = 'temperature_2m_mean',
+		title: str | None = '',
+		output_filename: Path | str | None = None,
+	):
+		if parameter not in df.columns:
+			raise ValueError(f"Parameter '{parameter}' not found in DataFrame.")
+		fig = plt.figure(figsize=(16, 8))
+		for city, city_data in df.groupby('city'):
+			plt.plot(
+				city_data['month'],
+				city_data[parameter],
+				label=city,
+				marker='o',
+				linestyle='-',
+			)
+		plt.title(
+			title if title else f'{parameter.capitalize()} Across Cities', fontsize=16
+		)
+		plt.xlabel('Month', fontsize=14)
+		plt.ylabel(parameter.replace('_', ' ').capitalize(), fontsize=14)
+		plt.legend(title='Cities', fontsize=12)
+		plt.grid()
+		plt.xticks(rotation=45)
+		plt.tight_layout()
+		if output_filename:
+			fig.savefig(output_filename)
 
 
-if __name__ == "__main__":
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    FIG_FOLDER = os.path.join(script_dir, "figs")
-    os.makedirs(FIG_FOLDER, exist_ok=True)
-    data = Meteo(["Madrid", "London", "Rio"]).data
-    parameters = ["temperature_2m_mean", "precipitation_sum", "wind_speed_10m_max"]
-    [Meteo.plot_weather_parameter(
-        data, 
-        parameter=parameter, 
-        output_filename=os.path.join(FIG_FOLDER, f"{parameter}.png")
-    ) for parameter in parameters]
+if __name__ == '__main__':
+	script_dir = os.path.dirname(os.path.abspath(__file__))
+	FIG_FOLDER = os.path.join(script_dir, 'figs')
+	os.makedirs(FIG_FOLDER, exist_ok=True)
+	data = Meteo(['Madrid', 'London', 'Rio']).data
+	parameters = ['temperature_2m_mean', 'precipitation_sum', 'wind_speed_10m_max']
+	[
+		Meteo.plot_weather_parameter(
+			data,
+			parameter=parameter,
+			output_filename=os.path.join(FIG_FOLDER, f'{parameter}.png'),
+		)
+		for parameter in parameters
+	]
--- tests/module_1/test_meteo_api.py
+++ tests/module_1/test_meteo_api.py
@@ -1,6 +1,5 @@
 """ This is a dummy example to show how to import code from src/ for testing"""
 
-from src.module_1.module_1_meteo_api import main
 
 
 def test_main():
